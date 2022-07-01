3b97cc24ffc37804234a8c6498318a933e7bcc56
ndom91 <yo@ndo.dev>
2022-07-01 23:16:45 +0200

    fix: set headers in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,20 @@ const nextConfig = {
     legacyBrowsers: false,
     browsersListForSwc: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/api/image',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://briefkasten.vercel.app',
+          },
+          { key: 'Access-Control-Allow-Methods', value: 'GET' },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
