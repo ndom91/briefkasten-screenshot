b4425074e0a3140bfbe2d5f37937f68494adb4d2
ndom91 <yo@ndo.dev>
2022-08-20 23:28:08 +0200

    feat: test playwright-core + node16 on vercel

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,12 +1,13 @@
 import chrome from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer-core'
+/* import puppeteer from 'puppeteer-core' */
+import playwright from 'playwright-core'
 import { serverTiming } from '@/lib/helpers'
 
 export default async function Image(req, res) {
   try {
     serverTiming.start()
     serverTiming.measure('browserStart')
-    const browser = await puppeteer.launch({
+    const browser = await playwright.chromium.launch({
       args: chrome.args,
       executablePath:
         process.env.NODE_ENV !== 'development'
@@ -16,11 +17,16 @@ export default async function Image(req, res) {
       ignoreHTTPSErrors: true,
     })
 
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 })
+    const page = await browser.newPage({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    })
+    /* await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 }) */
     serverTiming.measure('browserStart')
     serverTiming.measure('pageView')
-    await page.goto(req.query.url, { waitUntil: 'networkidle0' })
+    /* await page.goto(req.query.url, { waitUntil: 'networkidle0' }) */
+    await page.goto(req.query.url)
     serverTiming.measure('pageView')
 
     // Hack for accepting cookie banners
@@ -36,16 +42,20 @@ export default async function Image(req, res) {
       /(Accept all|I agree|Accept|Agree|Agree all|Ich stimme zu|Okay|OK)/
 
     serverTiming.measure('cookieHack')
-    const elements = await page.$$(selectors)
-    for (const el of elements) {
-      const innerText = (await el.getProperty('innerText')).toString()
-      regex.test(innerText, 'ig') && el.click()
+    console.log('selector', selectors.join(', '))
+    const elements = await page.$(`'${selectors.join(', ')}'`)
+    if (elements) {
+      console.log('els', elements)
+      for (const el of elements) {
+        const innerText = (await el.getProperty('innerText')).toString()
+        regex.test(innerText, 'ig') && el.click()
+      }
     }
 
     // Wait for cookie banner to be gone
-    await page.waitForNetworkIdle({
-      timeout: 25000,
-    })
+    /* await page.waitForNetworkIdle({ */
+    /*   timeout: 25000, */
+    /* }) */
 
     // Snap screenshot
     const buffer = await page.screenshot({ type: 'jpeg', quality: 50 })
