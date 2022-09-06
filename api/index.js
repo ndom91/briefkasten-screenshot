8c9c769c1c6c20c5ab8d07f3267e252b0b9b6253
ndom91 <yo@ndo.dev>
2022-09-06 23:21:33 +0200

    feat: alow POST requests to /api/iamge

--- api/index.js
+++ api/index.js
@@ -16,13 +16,116 @@ const resolveOrigin = (origin) => {
   return ''
 }
 
+const handleScreenshot = async ({ params, url }) => {
+  const { colorScheme, skipCookieBannerClick } = params
+
+  serverTiming.start()
+  serverTiming.measure('browserStart')
+  const browser = await chromium.launch({
+    args: chrome.args,
+    executablePath:
+      process.env.NODE_ENV !== 'development'
+        ? await chrome.executablePath
+        : '/bin/chromium',
+    headless: true,
+    ignoreHTTPSErrors: true,
+  })
+
+  const page = await browser.newPage({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  })
+  serverTiming.measure('browserStart')
+  serverTiming.measure('pageView')
+  if (colorScheme) {
+    await page.emulateMedia({ colorScheme })
+  }
+  await page.goto(url)
+  serverTiming.measure('pageView')
+
+  // Hack for accepting cookie banners
+  if (!skipCookieBannerClick) {
+    const selectors = [
+      '[id*=cookie] a',
+      '[class*=consent] button',
+      '[class*=cookie] a',
+      '[id*=cookie] button',
+      '[class*=cookie] button',
+    ]
+
+    const regex =
+      /(Accept all|I agree|Accept|Agree|Agree all|Ich stimme zu|Okay|OK)/
+
+    serverTiming.measure('cookieHack')
+    const elements = await page.$(`'${selectors.join(', ')}'`)
+    if (elements) {
+      for (const el of elements) {
+        const innerText = (await el.getProperty('innerText')).toString()
+        regex.test(innerText, 'ig') && el.click()
+      }
+    }
+
+    serverTiming.measure('cookieHack')
+  }
+  serverTiming.measure('screenshot')
+  // Snap screenshot
+  const buffer = await page.screenshot({ type: 'png' })
+  serverTiming.measure('screenshot')
+
+  await page.close()
+  await browser.close()
+
+  return buffer
+}
+
 app.get('/', async () => {
   return { hello: 'world' }
 })
 
+/**
+ * @type {import('fastify').RouteShorthandOptions}
+ */
 app.get('/api/image', async (req, reply) => {
   let { url } = req.query
-  const { colorScheme, skipCookieBannerClick } = req.query
+
+  if (req.headers.origin) {
+    reply.header(
+      'Access-Control-Allow-Origin',
+      resolveOrigin(req.headers.origin)
+    )
+  }
+
+  if (!url) {
+    url = req.body?.url ?? undefined
+    reply.header('Access-Control-Allow-Headers', 'Content-Type')
+    reply.header('Content-Type', 'application/json')
+    throw { statusCode: 400, message: 'Missing URL Param' }
+  }
+
+  try {
+    // Set the `s-maxage` property to cache at the CDN layer
+    reply.header('Cache-Control', 's-maxage=31536000, public')
+    reply.header('Content-Type', 'image/png')
+
+    // Generate Server-Timing headers
+    reply.header('Server-Timing', serverTiming.setHeader())
+
+    return handleScreenshot({ params: req.query, url })
+  } catch (e) {
+    console.error('Error generating screenshot -', e)
+    return JSON.stringify({
+      message: 'Image Capture Failed',
+      error: e,
+    })
+  }
+})
+
+/**
+ * @type {import('fastify').RouteShorthandOptions}
+ */
+app.post('/api/image', async (req, reply) => {
+  let { url } = req.query
 
   if (!url) {
     url = req.body?.url ?? undefined
@@ -40,70 +143,14 @@ app.get('/api/image', async (req, reply) => {
   }
 
   try {
-    serverTiming.start()
-    serverTiming.measure('browserStart')
-    const browser = await chromium.launch({
-      args: chrome.args,
-      executablePath:
-        process.env.NODE_ENV !== 'development'
-          ? await chrome.executablePath
-          : '/bin/chromium',
-      headless: true,
-      ignoreHTTPSErrors: true,
-    })
-
-    const page = await browser.newPage({
-      width: 1920,
-      height: 1080,
-      deviceScaleFactor: 1,
-    })
-    serverTiming.measure('browserStart')
-    serverTiming.measure('pageView')
-    if (colorScheme) {
-      await page.emulateMedia({ colorScheme })
-    }
-    await page.goto(req.query.url)
-    serverTiming.measure('pageView')
-
-    // Hack for accepting cookie banners
-    if (!skipCookieBannerClick) {
-      const selectors = [
-        '[id*=cookie] a',
-        '[class*=consent] button',
-        '[class*=cookie] a',
-        '[id*=cookie] button',
-        '[class*=cookie] button',
-      ]
-
-      const regex =
-        /(Accept all|I agree|Accept|Agree|Agree all|Ich stimme zu|Okay|OK)/
-
-      serverTiming.measure('cookieHack')
-      const elements = await page.$(`'${selectors.join(', ')}'`)
-      if (elements) {
-        for (const el of elements) {
-          const innerText = (await el.getProperty('innerText')).toString()
-          regex.test(innerText, 'ig') && el.click()
-        }
-      }
-
-      serverTiming.measure('cookieHack')
-    }
-    serverTiming.measure('screenshot')
-    // Snap screenshot
-    const buffer = await page.screenshot({ type: 'png' })
-    serverTiming.measure('screenshot')
-
-    await page.close()
-    await browser.close()
-
     // Set the `s-maxage` property to cache at the CDN layer
     reply.header('Cache-Control', 's-maxage=31536000, public')
     reply.header('Content-Type', 'image/png')
+
     // Generate Server-Timing headers
     reply.header('Server-Timing', serverTiming.setHeader())
 
-    return buffer
+    return handleScreenshot({ params: req.query, url })
   } catch (e) {
     console.error('Error generating screenshot -', e)
     return JSON.stringify({
